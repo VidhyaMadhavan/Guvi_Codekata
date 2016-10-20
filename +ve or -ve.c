e8a982f650a65eb2d5b7f02910e7be7b3a76fc1d
VidhyaMadhavan <vidhya.m1818@gmail.com>
2016-10-20 11:12:19 +0530

    Create +ve or -ve.c

---
+++ +ve or -ve.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int r;
+ clrscr();
+ printf("Enter the Number\n");
+ scanf("%d",&r);
+ if(r>0)
+ printf("\nEntered Number %d is +VE",r);
+ else if(r<0)
+ printf("\nEntered Number %d is -VE",r);
+ else
+ printf("\nEntered Number is Zero\n");
+ getch();
+ }
